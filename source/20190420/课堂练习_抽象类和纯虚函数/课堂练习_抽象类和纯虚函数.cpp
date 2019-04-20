01b431a56ae6e8b396f7e988e415655ab9846eca
Ratatouille <307338947@qq.com>
2019-04-20 18:44:05 +0800

    课堂练习 虚函数 多态

--- source/20190420/课堂练习_抽象类和纯虚函数/课堂练习_抽象类和纯虚函数.cpp
+++ source/20190420/课堂练习_抽象类和纯虚函数/课堂练习_抽象类和纯虚函数.cpp
@@ -7,6 +7,10 @@ class AbstractCPU
 {
 public:
 	virtual void CpuCalc() = 0;
+	virtual ~AbstractCPU()
+	{
+		cout << "Abstruct CPU destructed" << endl;
+	}
 };
 
 
@@ -14,6 +18,10 @@ class AbstractGPU
 {
 public:
 	virtual void GpuCalc() = 0;
+	virtual ~AbstractGPU()
+	{
+		cout << "Abstruct GPU destructed" << endl;
+	}
 };
 
 
@@ -21,30 +29,49 @@ class AbstractMEM
 {
 public:
 	virtual void MEMRun() = 0;
+	virtual ~AbstractMEM()
+	{
+		cout << "Abstruct MEM destructed" << endl;
+	}
 };
 
 class IntelCPU : public AbstractCPU
 {
+public:
 	virtual void CpuCalc()
 	{
 		cout << "Intel CPU Calculating !" << endl;
+	}
+	~IntelCPU()
+	{
+		cout << "intel CPU runs no more" << endl;
 	}
 };
 
 class NvidiaGPU : public AbstractGPU
 {
+public:
 	virtual void GpuCalc()
 	{
 		cout << "NVidia GPU Calculating !" << endl;
+	}
+	~NvidiaGPU()
+	{
+		cout << "NV gpu runs no more" << endl;
 	}
 
 };
 
 class UniMEM : public AbstractMEM
 {
+public:
 	virtual void MEMRun()
 	{
 		cout << "Uni MEM Runs !" << endl;
+	}
+	~UniMEM()
+	{
+		cout << "uni mem runs no more" << endl;
 	}
 	
 
@@ -70,6 +97,12 @@ public:
 		gpu->GpuCalc();
 		mem->MEMRun();
 	}
+	~Computer()
+	{
+		delete cpu;
+		delete gpu;
+		delete mem;
+	}
 
 };
 
@@ -81,6 +114,7 @@ void test01()
 
 	Computer* computer = new Computer(intel_cpu, nvidia_gpu, uni_mem);
 	computer->ComputerRuns();
+	delete computer;
 }
 int main()
 {
