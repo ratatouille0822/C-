91157700d1b3e507cc129c91ffb565ec4beca0e1
Ratatouille <307338947@qq.com>
2019-04-20 16:20:03 +0800

    课堂练习

---
+++ source/20190420/课堂练习_抽象类和纯虚函数/课堂练习_抽象类和纯虚函数.cpp
@@ -0,0 +1,106 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+class AbstractCPU
+{
+public:
+	virtual void CpuCalc() = 0;
+};
+
+
+class AbstractGPU
+{
+public:
+	virtual void GpuCalc() = 0;
+};
+
+
+class AbstractMEM
+{
+public:
+	virtual void MEMRun() = 0;
+};
+
+class IntelCPU : public AbstractCPU
+{
+	virtual void CpuCalc()
+	{
+		cout << "Intel CPU Calculating !" << endl;
+	}
+};
+
+class NvidiaGPU : public AbstractGPU
+{
+	virtual void GpuCalc()
+	{
+		cout << "NVidia GPU Calculating !" << endl;
+	}
+
+};
+
+class UniMEM : public AbstractMEM
+{
+	virtual void MEMRun()
+	{
+		cout << "Uni MEM Runs !" << endl;
+	}
+	
+
+};
+
+class Computer
+{
+public:
+	Computer(AbstractCPU* cpu, AbstractGPU* gpu, AbstractMEM* mem)
+	{
+		this->cpu = cpu;
+		this->gpu = gpu;	
+		this->mem = mem;
+	}
+
+	AbstractCPU* cpu;
+	AbstractGPU* gpu;
+	AbstractMEM* mem;
+
+	void ComputerRuns()
+	{
+		cpu->CpuCalc();
+		gpu->GpuCalc();
+		mem->MEMRun();
+	}
+
+};
+
+void test01()
+{
+	IntelCPU* intel_cpu = new IntelCPU;
+	NvidiaGPU* nvidia_gpu = new NvidiaGPU;
+	UniMEM* uni_mem = new UniMEM;
+
+	Computer* computer = new Computer(intel_cpu, nvidia_gpu, uni_mem);
+	computer->ComputerRuns();
+}
+int main()
+{
+	test01();
+    std::cout << "Hello World!\n"; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
